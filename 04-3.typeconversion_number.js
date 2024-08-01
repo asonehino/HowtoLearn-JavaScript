2f0bf2f9297b2ffb14f64fb82832b6310d968cf7
hino <asonehino@naver.com>
2024-08-02 02:32:53 +0900

    typeconversion

---
+++ 04-3.typeconversion_number.js
@@ -0,0 +1,6 @@
+console.log (
+  Number("1234"),
+  //Number("abc"), --> NaN
+  Number(true),
+  Number(false)
+)
---
+++ 04-4.typeconversion_boolean.js
@@ -0,0 +1,11 @@
+console.log(
+  //숫자 0
+  //빈 문자열 ""
+  //null
+  //undefined
+  //NaN 
+  //을 제외하면 전부 true로 반환한다
+  Boolean(0),
+  Boolean(1),
+  Boolean("java")
+)
