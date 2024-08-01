f747a4518e292807113cc5ca24157e1a703e62f3
hino <asonehino@naver.com>
2024-08-02 02:16:57 +0900

    prompt

--- 03-1.prompt.js
+++ 03-1.prompt.js
@@ -1,2 +1,6 @@
-const name = prompt("이름을 입력하세요.");
-alert("환영합니다, " + name + "님");
+//nodejs에서는 prompt를 지원하지 않기 때문에
+//prompt-sync 모듈이 필요하다
+const prompt = require('prompt-sync')({sigint:true});
+
+const name = prompt("이름을 입력하세요:");
+console.log("환영합니다, " + name + "님");
