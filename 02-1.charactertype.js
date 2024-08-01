a497cb9aa9c7fdd29532c27ada80818ce41451ca
hino <asonehino@naver.com>
2024-08-01 23:55:35 +0900

    character type

---
+++ 02-1.charactertype.js
@@ -0,0 +1,20 @@
+const name = "hyeonho";
+const age = "18";
+
+const name1 = "hyeonho";
+const name2 = 'hyeonho';
+const name3 = `hyeonho`;
+
+const message1 = "I'm a boy.";
+const message2 = 'I\'m a boy.';
+const message3 = 'My name is ${name}';
+const message4 = '나는 ${10+8}살 입니다.';
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
+console.log(message4);
