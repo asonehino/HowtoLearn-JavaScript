c13e5b5205557a56176f384137d9af5a53ca0896
hino <asonehino@naver.com>
2024-08-03 00:28:11 +0900

    start array

---
+++ 13-1.array.js
@@ -0,0 +1,15 @@
+let arr = [
+  '현호', 3, true, 
+  {
+    name: 'hyeonho', 
+    age: 18,
+  }, 
+  function() {
+    console.log('test');
+  }
+]
+
+arr.push('부산소프트웨어마이스터고');
+
+console.log(arr);
+console.log(arr.length);
